4479c828693e680bc57db31971dd992b44b8d601
Ismael Bueno <ismaelwno@gmail.com>
2023-04-29 01:14:52 +0200

    Added pch files

---
+++ include/ppx-app/pch.hpp
@@ -0,0 +1,14 @@
+#ifndef PPX_APP_PCH
+#define PPX_APP_PCH
+
+#include <string>
+#include <vector>
+#include <functional>
+#include <SFML/Graphics.hpp>
+#include "imgui.h"
+#include "imgui-SFML.h"
+#include <filesystem>
+#include <glm/geometric.hpp>
+#include <glm/gtx/norm.hpp>
+
+#endif
---
+++ src/pch.cpp
@@ -0,0 +1,1 @@
+#include "ppx-app/pch.hpp"
