730fc4a2922348ece8130f14c9850a233ffd3168
Ismael Bueno <ismaelwno@gmail.com>
2023-04-28 19:32:12 +0200

    layer has now a cpp file with the write/read definitions

---
+++ src/layer.cpp
@@ -0,0 +1,9 @@
+#include "ppx/layer.hpp"
+
+namespace ppx
+{
+    layer::layer(const char *name) : m_name(name) {}
+
+    void layer::write(ini::output &out) const { out.write("enabled", p_enabled); }
+    void layer::read(ini::input &in) { p_enabled = (bool)in.readi16("enabled"); }
+}
