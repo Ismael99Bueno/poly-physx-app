f14df64cfa46659a3d10c28fc4aeb99025d5b306
Ismael Bueno <ismaelwno@gmail.com>
2023-08-26 17:20:05 +0100

    Silly glm defines

--- include/ppx-app/internal/pch.hpp
+++ include/ppx-app/internal/pch.hpp
@@ -6,6 +6,8 @@
 #include <functional>
 #include <imgui.h>
 #include <filesystem>
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
 #include <glm/geometric.hpp>
 #include <glm/gtx/norm.hpp>
 #ifdef LYNX_ENABLE_IMGUI
