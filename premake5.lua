286b18091f41cbe341b695912783b493681b0ed6
Ismael Bueno <ismaelwno@gmail.com>
2023-12-23 21:21:28 +0000

    Explicitly removed annoying sign conversion warning

--- premake5.lua
+++ premake5.lua
@@ -11,7 +11,8 @@ filter "system:macosx"
       "-Wextra",
       "-Wpedantic",
       "-Wconversion",
-      "-Wno-unused-parameter"
+      "-Wno-unused-parameter",
+      "-Wno-sign-conversion"
    }
 filter {}
 
