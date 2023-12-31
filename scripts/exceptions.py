3a33b22e224a29fe494e2f1ddb86235f0bf3fd7f
Ismael Bueno <ismaelwno@gmail.com>
2023-12-31 03:01:43 +0000

    Deleted old scripts folder

--- scripts/exceptions.py
+++
@@ -1,7 +0,0 @@
-class PathNotFoundError(Exception):
-    def __init__(self, path: str) -> None:
-        self.__path = path
-        super().__init__(self.__repr__())
-
-    def __repr__(self) -> str:
-        return f"{self.__path} not found. The project's folder structure may have been changed"
